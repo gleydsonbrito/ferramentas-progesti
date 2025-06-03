9e42119991a8ca9e903c9bae85561d430a1d655c
gleydsonbrito <gleydsonbrito@hotmail.com>
2025-06-03 20:15:51 -0300

    ajusta validação do usuário logado

--- scripts/index.js
+++ scripts/index.js
@@ -10,25 +10,26 @@ loginForm.addEventListener('submit', function (e) {
     .then(res => res.json())
     .then(res => {
       const findedUser = res.usuarios.find(u => u.login === inputEmail)
-      if (findedUser) {
-        res.usuarios.forEach(usuario => {
-          if (usuario.login === inputEmail) {
-            const sHASH = CryptoJS.MD5(inputPassword).toString()
-            if (usuario.senha === sHASH) {
-              const nomeSobrenome = usuario.login.split('@')[0]
-              const [nome, sobrenome] = nomeSobrenome.split('.')
-              const perfil = {
-                nome,
-                sobrenome,
-                "email": usuario.login
-              }
-              localStorage.setItem('perfil', JSON.stringify(perfil))
-              window.location.href = './pages/dashboard.html';
-            }
-          }
-        });
-      } else {
+      if (!findedUser) {
         alert(`O usuário ${inputEmail} não está cadastrado no sistema.`)
+        return
       }
+
+      res.usuarios.forEach(usuario => {
+        if (usuario.login === inputEmail) {
+          const sHASH = CryptoJS.MD5(inputPassword).toString()
+          if (usuario.senha === sHASH) {
+            const nomeSobrenome = usuario.login.split('@')[0]
+            const [nome, sobrenome] = nomeSobrenome.split('.')
+            const perfil = {
+              nome,
+              sobrenome,
+              "email": usuario.login
+            }
+            localStorage.setItem('perfil', JSON.stringify(perfil))
+            window.location.href = './pages/dashboard.html';
+          }
+        }
+      });
     })
 });
